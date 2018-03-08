65ad287cd55ec3d379eda81febafbd43c6d5416a
Kevin Meziere <kevinmeziere@users.noreply.github.com>
2018-03-08 09:57:37 -0800

    Create cleanser.sh

---
+++ cleanser.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/bash
+
+#
+#
+# Script to remove out chunter, and zlogin from a gz
+# 
+# WARNING
+# There are plenty of ways this could go wrong!
+#
+#
+
+cd /opt/
+rm -rf *
+svcadm disable chunter
+svcadm disable zlogin
+svcadm disable empd
+
+
+for pid in $(ps -ef | awk '/chunter/ {print $2}'); do kill -9 $pid; done
+for pid in $(ps -ef | awk '/zlogin/ {print $2}'); do kill -9 $pid; done
+for pid in $(ps -ef | awk '/empd/ {print $2}'); do kill -9 $pid; done
+for pid in $(ps -ef | awk '/erlang/ {print $2}'); do kill -9 $pid; done
